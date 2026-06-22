cbcd84c609677618ff5bceb5ad497f15554d1e92
Jack Mulqueeney <jmulqueeney@uchicago.edu>
2026-06-22 13:02:49 -0500

    imrpove country prefix removal cleanup

--- code/2_process_open_tender.R
+++ code/2_process_open_tender.R
@@ -216,4 +216,6 @@ winner_data_long <- winner_data_long %>%
 # e.g. bidder_country = "DK" and bidder_bodyIds = "DK12345678"
 # or bidder_country = "SE" and bidder_bodyIds = "SE123456789"
 winner_data_long <- winner_data_long %>% 
-  mutate(bidder_bodyIds = gsub(bidder_country, "", bidder_bodyIds))
+  mutate(bidder_bodyIds = if_else(str_sub(bidder_bodyIds, start = 1, end = 2) == bidder_country,
+                                  substring(bidder_bodyIds, first = 3),
+                                  bidder_bodyIds))
